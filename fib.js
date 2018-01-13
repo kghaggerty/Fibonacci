3c76fa27c61c34f8f3f49f4ba728f12d52322cd5
Kevin Haggerty <kghaggerty@gmail.com>
2018-01-13 14:44:29 -0600

    fib exercise

---
+++ fib.js
@@ -0,0 +1,13 @@
+//list of fib numbers less than 500  [1,1,2,3,5,8,13,21..]
+
+let a = 0
+let b = 1
+let sum = ""
+
+for (let index = 0; index < 5; index++) {
+    let sum = a + b
+    console.log(sum);
+    a = b;
+    b = sum;
+    
+}

b5b34c2cb1aa3c2081ec01c048880262a5fe6133
Kevin Haggerty <kghaggerty@gmail.com>
2018-01-31 11:47:21 -0600

    added palindrome exercise

--- fib.js
+++ fib.js
@@ -1,13 +1,20 @@
-//list of fib numbers less than 500  [1,1,2,3,5,8,13,21..]
+//list of fib numbers
 
 let a = 0
 let b = 1
-let sum = ""
+let sum = []
 
-for (let index = 0; index < 13; index++) {
-    let sum = a + b
-    console.log(sum);
-    a = b;
-    b = sum;
-    
-}
+for (let index = 0; index < 10; index++) {
+    let c = a + b
+    sum.push(c + "")
+    a = b
+    b = c
+  }
+  console.log(sum)
+
+//compare palindrones 
+  let radar = "radar"
+  let newRadar = radar.split("").reverse().join('')
+  if(radar === newRadar){
+      console.log("success!")
+  }
